15fc6c13af4848f6d32289182cd2ede6777ca7f4
bertyn <bertynboulikou@gmail.com>
2021-05-25 15:21:24 +0200

    ajout du composant base-button

---
+++ components/BaseButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button :href="url" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "BaseButton",
+  props: ["url", "text"]
+};
+</script>
+
+<style></style>
