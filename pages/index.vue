49fcf78271a97ce5f105f834db245ea88057b16c
bertyn <bertynboulikou@gmail.com>
2021-05-25 17:53:28 +0200

    retrait des ancien composant nuxt

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,9 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
       <h1 class="title">
-        HAWABOL'S-website
+        Test composant
       </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
