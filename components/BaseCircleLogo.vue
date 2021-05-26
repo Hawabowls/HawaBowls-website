75b8ed47517ddc3b7cfbd5a068681467fa396b48
bertyn <bertynboulikou@gmail.com>
2021-05-26 12:04:23 +0200

    ajout du composant circlelogo

---
+++ components/BaseCircleLogo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <img class="h-20 object-cover" src="img/Fichier 3.png" alt="Hawabowls" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
