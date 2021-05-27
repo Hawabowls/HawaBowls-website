cd03d99b27cc4622518efe869f976f871d587731
bertyn <bertynboulikou@gmail.com>
2021-05-27 11:37:12 +0200

    ajout de composant du vecteur triangle

---
+++ components/VectorBackground.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="relative w-full h-44 ">
+    <svg
+      class="absolute inset-x-0 fill-current text-primary-200 top-0 transform rotate-180"
+      viewBox="0 0 1438 229"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M1438 0V229H0L1438 0Z" />
+    </svg>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "VectorBacground"
+};
+</script>
+
+<style></style>
