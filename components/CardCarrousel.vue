eb44a210f3cebf265dec5aa4d3096d084cbed445
bertyn <bertynboulikou@gmail.com>
2021-05-27 09:15:59 +0200

    ajout de carrousel de news

---
+++ components/CardCarrousel.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="max-w-xs mx-auto bg-white rounded-md shadow-md">
+    <div class="h-64">
+      <img
+        class="w-full rounded-t shadow h-full object-cover object-center   "
+        :src="post.url"
+        alt=""
+      />
+    </div>
+    <div class="py-2">
+      <span class="text-accent-200 ">{{ post.source }}</span>
+      <!-- <h3 class="font-semibold text-gray-800">{{ post.titre }}</h3> -->
+      <p class="text-gray-600">{{ post.caption }}</p>
+    </div>
+    <div class="flex justify-around pb-1">
+      <div class="fill-accent-500">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="#ffffff"
+          stroke="#f8b04d"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+            clip-rule="evenodd"
+            stroke-width="2"
+          />
+        </svg>
+        <span>
+          {{ post.like }}
+        </span>
+      </div>
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="#f8b04d"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+          />
+        </svg>
+
+        <span>{{ post.comment }}</span>
+      </div>
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="#f8b04d"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
+          />
+        </svg>
+
+        <span>{{ post.share }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardCarrousel",
+  props: ["post", "source"]
+};
+</script>
+
+<style></style>
---
+++ components/TheBestDishes.vue
@@ -0,0 +1,97 @@
+<template>
+  <section class="container">
+    <splide :options="options">
+      <splide-slide v-for="(news, i) in lastNews" :key="i">
+        <card-carrousel post="news" class="mb-8"></card-carrousel>
+      </splide-slide>
+    </splide>
+  </section>
+</template>
+
+<script>
+import CardCarrousel from "./CardCarrousel.vue";
+export default {
+  components: { CardCarrousel },
+  data() {
+    return {
+      options: {
+        rewind: true,
+        padding: 10,
+        arrows: "slide",
+        width: 900,
+        perPage: 3,
+        gap: "2rem",
+        breakpoints: {
+          650: {
+            perPage: 1,
+            width: 580
+          },
+          1024: {
+            perPage: 2,
+            width: 780
+          }
+        }
+      },
+
+      lastNews: [
+        {
+          id: 0,
+          url:
+            "https://images.unsplash.com/photo-1556040220-4096d522378d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          titre: "Hola que tal",
+          caption: "Bienvenido",
+          source: "blog"
+        },
+        {
+          id: 1,
+          url:
+            "https://images.unsplash.com/photo-1556040220-4096d522378d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          titre: "Poke Poke Poke",
+          caption:
+            "dcsbdcqhijkejxwhmlzdUIFBDCQJKXWGUKEFJBCSDKHQIODEDbsvisdb<lqsk",
+          source: "instragram"
+        },
+        {
+          id: 2,
+          url:
+            "https://images.unsplash.com/photo-1556040220-4096d522378d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          titre: "Hola que tal",
+          caption:
+            "dcsbdcqhijkejxwhmlzdUIFBDCQJKXWGUKEFJBCSDKHQIODEDbsvisdb<lqsk",
+          source: "blog"
+        },
+        {
+          id: 3,
+          url:
+            "https://images.unsplash.com/photo-1556040220-4096d522378d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          titre: "Poke Poke Poke",
+          caption:
+            "dcsbdcqhijkejxwhmlzdUIFBDCQJKXWGUKEFJBCSDKHQIODEDbsvisdb<lqsk",
+          source: "instragram"
+        },
+
+        {
+          id: 4,
+          url:
+            "https://images.unsplash.com/photo-1556040220-4096d522378d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          titre: "Poke Poke Poke",
+          caption:
+            "dcsbdcqhijkejxwhmlzdUIFBDCQJKXWGUKEFJBCSDKHQIODEDbsvisdb<lqsk",
+          source: "instragram"
+        },
+        {
+          id: 5,
+          url:
+            "https://images.unsplash.com/photo-1556040220-4096d522378d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+          titre: "Poke Poke Poke",
+          caption:
+            "dcsbdcqhijkejxwhmlzdUIFBDCQJKXWGUKEFJBCSDKHQIODEDbsvisdb<lqsk",
+          source: "instragram"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
