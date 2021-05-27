7d4f0db8bfd122a7c4fdf7dded4f8203de35e59f
bertyn <bertynboulikou@gmail.com>
2021-05-27 16:04:51 +0200

    ajout des card with text

---
+++ components/CardWithText.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex  max-w-sm m-2 items-center py-4 px-1">
+    <div class="w-32 h-24 bg-gray-400 mx-1">
+      <slot></slot>
+    </div>
+    <div>
+      <span>{{ titre }}</span>
+      <p class="text-center leading-tight">
+        {{ text }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["titre", "text"]
+};
+</script>
+
+<style></style>
