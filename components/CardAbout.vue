993bca3aeba7ea27920ccebaa0434ee4e8b7a5c7
bertyn <bertynboulikou@gmail.com>
2021-05-27 17:37:39 +0200

    card

---
+++ components/CardAbout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="max-w-xs bg-white rounded-lg shadow-lg opacity-80 h-64 m-1 py-2 px-4 flex flex-col place-content-center "
+  >
+    <div class="my-2">
+      <slot></slot>
+    </div>
+    <span class="leading font-normal text-center text-gray-700">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum veritatis
+      magnam. Reprehenderit consequatur ducimus id libero. Provident, magni.
+    </span>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
