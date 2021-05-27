69681b99dedaa0e6b82ca4e9d0fe8d19ed3c25cf
bertyn <bertynboulikou@gmail.com>
2021-05-27 16:05:13 +0200

    ajout du favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,10 +8,10 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Restaurant de Poke' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: 'img/Fichier 1.png' }
     ]
   },
 
