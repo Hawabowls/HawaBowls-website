999b5e2ec86d04b271306460861bb13b5dca6f36
bertyn <bertynboulikou@gmail.com>
2021-06-11 10:38:46 +0200

    ajout de librairie

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/vue-splide.client.js'
+    { src: '~/plugins/vue-splide.js', mode: 'client' },
+
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -52,7 +53,7 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
+      lang: 'fr'
     }
   },
 
