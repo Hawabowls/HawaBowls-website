a6c248f97258efcb9f217f67de097b48e7ce752f
bertyn <bertynboulikou@gmail.com>
2021-06-15 11:33:46 +0200

    gestion des roles

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
     title: 'hawabowls-website',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.1' },
       { hid: 'description', name: 'description', content: 'Restaurant de Poke' }
     ],
     link: [
@@ -22,7 +22,6 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     { src: '~/plugins/vue-splide.js', mode: 'client' },
-
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -32,6 +31,7 @@ export default {
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
+
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
