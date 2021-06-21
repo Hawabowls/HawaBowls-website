48bfe054a19cdd5220fcc8de011096890b6cf3e7
bertyn <bertynboulikou@gmail.com>
2021-06-21 11:48:39 +0200

    correction couleur fond loading

--- components/LoadingIcon.vue
+++ components/LoadingIcon.vue
@@ -29,7 +29,7 @@ export default {
   left: 0;
   width: 100%;
   height: 100%;
-  background: rgba(255, 255, 255, 1);
+  background: #30377f;
   z-index: 3000;
   text-align: center;
   display: flex;
