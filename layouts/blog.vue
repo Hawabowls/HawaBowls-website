d02f69d6e2a256053bb40a5cbd1eeb67d0429b5a
bertyn <bertynboulikou@gmail.com>
2021-06-27 17:02:17 +0200

    ajout composant pagination et du layout blog

---
+++ layouts/blog.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <the-navbar></the-navbar>
+    <Nuxt />
+    <the-footer></the-footer>
+  </div>
+</template>
+
+<script>
+import TheFooter from "~/components/TheFooter.vue";
+import TheNavbar from "~/components/TheNavbar.vue";
+export default {
+  components: { TheNavbar, TheFooter }
+};
+</script>
+
+<style lang="postcss">
+::-webkit-scrollbar {
+  width: 11px;
+
+  &-track {
+    @apply bg-primary-200 rounded;
+  }
+  &-thumb {
+    @apply rounded-full bg-primary-500;
+    &:hover {
+      @apply bg-primary-600;
+    }
+  }
+}
+</style>
