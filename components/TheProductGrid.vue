00037c0182723c82c078daae9c7c61102ecfcede
bertyn <bertynboulikou@gmail.com>
2021-06-28 11:27:56 +0200

    ajout block  a productgrid

--- components/TheProductGrid.vue
+++ components/TheProductGrid.vue
@@ -29,20 +29,49 @@
           alt=""
         />
       </div>
-      <div class="h-72 w-full bg-primary-400">
+      <div
+        class="
+          h-72
+          w-full
+          bg-primary-100
+          flex
+         flex-col
+          items-center
+          justify-center
+        "
+      >
+        <h3 class="animate-ping text-accent-400 text-4xl font-bold">
+          Poke
+        </h3>
+      </div>
+      <!--   <div class="h-72 w-full bg-primary-400">
         <img
           class="h-full w-full object-cover"
           src="https://images.unsplash.com/photo-1544551763-77ef2d0cfc6c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZmlzaHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60
           "
           alt=""
         />
-      </div>
-      <div class="hidden lg:block h-72 w-full bg-primary-600">
+      </div> -->
+      <!-- <div class="hidden lg:block h-72 w-full bg-primary-600">
         <img
           class="h-full w-full object-cover"
           src="https://picsum.photos/seed/picsum/300/300"
           alt=""
         />
+      </div> -->
+      <div
+        class="hidden lg:flex
+          h-72
+          w-full
+          bg-accent-100
+         flex-col
+          items-center
+          justify-center
+        "
+      >
+        <h3 class="animate-pulse text-accent-400 text-4xl font-bold">
+          Fresh
+        </h3>
       </div>
       <div class="hidden lg:block h-72 w-full bg-primary-600">
         <img
