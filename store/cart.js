4067b7573e3705500aab8e7c831cf1800fe21634
bertyn <bertynboulikou@gmail.com>
2021-06-29 12:32:16 +0200

    modif store user et cart

--- store/cart.js
+++ store/cart.js
@@ -15,6 +15,7 @@ export const getters = {
     dishProducts: (state) => state.dishProducts,
     drinkProducts: (state) => state.drinkProducts,
     getCart: (state) => state.cartItems,
+    getTotalItemPrice: (state) => (id) => state.cartItems[id].price * state.cartItems[id].quantity,
     getCartTotal: (state) =>
         state.cartItems.length < 1
             ? '0'
@@ -23,6 +24,10 @@ export const getters = {
                 .reduce((a, b) => a + b),
 }
 export const mutations = {
+    removeItem: (state, item) => {
+        let id = state.cartItems.findIndex(x => x._id == item._id)
+        state.cartItems.splice(id, 1)
+    },
     setProducts: (state, products) => (state.allProducts = products),
     setFeaturedProducts: (state, products) => (state.featuredProducts = products),
     setDishProducts: (state, products) => (state.dishProducts = products),
@@ -30,15 +35,15 @@ export const mutations = {
     setCartItem: (state, item) => {
 
         if (state.cartItems.filter(i => i._id == item._id).length == 0) {
+            item.quantity = 1
             console.log(item)
             state.cartItems.push(item)
-            localStorage.setItem('cartItems', JSON.stringify(state.cartItems))
+
         } else {
             let index = state.cartItems.findIndex(x => x._id == item._id)
             console.log(index)
             console.log(state.cartItems[index])
             state.cartItems[index].quantity++
-            localStorage.setItem('cartItems', JSON.stringify(state.cartItems))
         }
     },
     removeCartItem: (state, id) =>
--- store/user.js
+++ store/user.js
@@ -1,26 +1,49 @@
 export const state = () => ({
-    user: {},
-    token: {}
+    loggedUser: null,
+    token: ""
 })
 
 export const getters = {
-    getUser: (state) => state.user,
+    getUser: (state) => state.loggedUser,
+
 
 }
 
-export const mutation = {}
+export const mutations = {
+    setCurrentToken: (state, token) => {
+        state.token = token
+    },
+    setCurrentUser: (state, user) => state.loggedUser = user
+}
 
 export const actions = {
-    async login(payload) {
+    async login({ commit }, form) {
         try {
-            const client = await this.$axios.post('api/product')
+            const client = await this.$axios.post('/api/login', form)
+            commit('setCurrentUser', client.data.user)
+            commit('setCurrentToken', client.data.token)
+            return client
         } catch (error) {
             return { error: error.message }
         }
     },
-    async register(payload) {
+    async register({ commit }, form) {
         try {
-            const client = await this.$axios.post('/api/register', payload)
+            console.log(form)
+            const client = await this.$axios.post('/api/register', form)
+            commit('setCurrentUser', client.data.user)
+            commit('setCurrentToken', client.data.token)
+            return client
+        } catch (error) {
+            console.log(error)
+            return { error: error.message }
+        }
+
+    },
+    async update({ commit }, id, form) {
+        try {
+            console.log(form)
+            const client = await this.$axios.post(`/api/${id}`, form)
             console.log(client)
             return client
         } catch (error) {
@@ -29,6 +52,33 @@ export const actions = {
         }
 
     },
-    update() { },
-    delUser: (state) => state.user = {},
+
+    async signOutUser({ commit }) {
+        try {
+
+            const client = await this.$axios.post(`/api/${id}`)
+        } catch (error) {
+            console.log(error)
+            return { error: error.message }
+        }
+        commit('setCurrentToken', "");
+        commit('setCurrentUser', null)
+    },
+
+    async reconnection({ commit, state }) {
+        try {
+
+            const client = await this.$axios.get(`/api/reconnect`, {
+                headers: {
+                    Authorization: `Bearer ${state.token}`
+                }
+            })
+            commit('setCurrentToken', client.data.token);
+            commit('setCurrentUser', client.data.user)
+
+        } catch (error) {
+            console.log(error)
+            return { error: error.message }
+        }
+    }
 }
