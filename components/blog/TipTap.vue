84a174db2a4a7f67fd08fef95fa40f6c6ac3f4c8
bertyn <bertynboulikou@gmail.com>
2021-06-29 17:07:34 +0200

    ajout éditeur pour article blog

---
+++ components/blog/TipTap.vue
@@ -0,0 +1,31 @@
+<template>
+  <editor-content :editor="editor" />
+</template>
+
+<script>
+import { Editor, EditorContent } from "@tiptap/vue-2";
+import StarterKit from "@tiptap/starter-kit";
+
+export default {
+  components: {
+    EditorContent
+  },
+
+  data() {
+    return {
+      editor: null
+    };
+  },
+
+  mounted() {
+    this.editor = new Editor({
+      content: "<p>I’m running tiptap with Vue.js. 🎉</p>",
+      extensions: [StarterKit]
+    });
+  },
+
+  beforeDestroy() {
+    this.editor.destroy();
+  }
+};
+</script>
