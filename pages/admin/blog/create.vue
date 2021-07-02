dbaa00eb8f05e11177738b6965f67d6ee0b6b24d
bertyn <bertynboulikou@gmail.com>
2021-07-02 23:08:43 +0200

    ajout de tiptap

--- pages/admin/blog/create.vue
+++ pages/admin/blog/create.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>
+  <main>
+    <div class="relative bg-secondary-500 md:pt-32 pb-32 z-0 pt-12">
+      <div class="px-4 md:px-10 mx-auto w-full"></div>
+    </div>
     <client-only>
       <tiptap />
     </client-only>
-  </div>
+  </main>
 </template>
 
 <script>
