cc7180db1515b46d45791b064ef8c8463cc4b5a7
bertyn <bertynboulikou@gmail.com>
2021-07-02 12:05:48 +0200

    modification head page  cgv et cgu

--- pages/cgu.vue
+++ pages/cgu.vue
@@ -1,8 +1,38 @@
 <template><div></div></template>
 
 <script>
-export default {};
+export default {
+  head() {
+    const meta = [
+      {
+        property: "og:title",
+        content: "Condition génerale d'utilisation",
+        hid: "og:title"
+      },
+      {
+        property: "og:description",
+        content:
+          "Condition génerale d'utilisation du système de le click n collect",
+        hid: "og:description"
+      },
+      {
+        property: "description",
+        content:
+          "Condition génerale d'utilisation du système de le click n collect",
+        hid: "description"
+      }
+    ];
+    meta.push({
+      property: "og:image",
+      content: "www.hawabowls.com/img/Fichier 3.png",
+      hid: "og:image"
+    });
+    return {
+      title: this.article.title,
+      meta
+    };
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>
--- pages/cgv.vue
+++ pages/cgv.vue
@@ -1,8 +1,38 @@
 <template></template>
 
 <script>
-export default {};
+export default {
+  head() {
+    const meta = [
+      {
+        property: "og:title",
+        content: "Condition génerale de vente",
+        hid: "og:title"
+      },
+      {
+        property: "og:description",
+        content:
+          "Condition génerale de vente des produits dans le click n collect",
+        hid: "og:description"
+      },
+      {
+        property: "description",
+        content:
+          "Condition génerale de vente des produits dans le click n collect",
+        hid: "description"
+      }
+    ];
+    meta.push({
+      property: "og:image",
+      content: "www.hawabowls.com/img/Fichier 3.png",
+      hid: "og:image"
+    });
+    return {
+      title: this.article.title,
+      meta
+    };
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>
