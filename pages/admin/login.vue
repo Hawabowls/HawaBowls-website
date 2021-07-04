88409502e60847a32e6a51c4c3abe4f773dcb982
bertyn99 <bboulikou@gmail.com>
2021-07-04 15:27:06 +0200

    comm de mansour

--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -1,6 +1,16 @@
 <template>
   <div
-    class="flex max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl"
+    class="
+      flex
+      max-w-sm
+      mx-auto
+      overflow-hidden
+      bg-white
+      rounded-lg
+      shadow-lg
+      dark:bg-gray-800
+      lg:max-w-4xl
+    "
   >
     <div
       class="hidden bg-cover lg:block lg:w-1/2 t object-cover"
@@ -15,37 +25,25 @@
       </h2>
 
       <p class="text-xl text-center text-gray-600 dark:text-gray-200">
-        Bienvenu
+        Bienvenu menbres du Staff
       </p>
 
       <a
         href="#"
-        class="flex items-center justify-center mt-4 text-gray-600 rounded-lg shadow-md dark:bg-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-600"
+        class="
+          flex
+          items-center
+          justify-center
+          mt-4
+          text-gray-600
+          rounded-lg
+          shadow-md
+          dark:bg-gray-700
+          dark:text-gray-200
+          hover:bg-gray-100
+          dark:hover:bg-gray-600
+        "
       >
-        <!--  <div class="px-4 py-3">
-          <svg class="w-6 h-6" viewBox="0 0 40 40">
-            <path
-              d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
-              fill="#FFC107"
-            />
-            <path
-              d="M5.25497 12.2425L10.7308 16.2583C12.2125 12.59 15.8008 9.99999 20 9.99999C22.5491 9.99999 24.8683 10.9617 26.6341 12.5325L31.3483 7.81833C28.3716 5.04416 24.39 3.33333 20 3.33333C13.5983 3.33333 8.04663 6.94749 5.25497 12.2425Z"
-              fill="#FF3D00"
-            />
-            <path
-              d="M20 36.6667C24.305 36.6667 28.2167 35.0192 31.1742 32.34L26.0159 27.975C24.3425 29.2425 22.2625 30 20 30C15.665 30 11.9842 27.2359 10.5975 23.3784L5.16254 27.5659C7.92087 32.9634 13.5225 36.6667 20 36.6667Z"
-              fill="#4CAF50"
-            />
-            <path
-              d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z"
-              fill="#1976D2"
-            />
-          </svg>
-        </div>
-
-        <span class="w-5/6 px-4 py-3 font-bold text-center"
-          >Sign in with Google</span
-        > -->
       </a>
 
       <div class="flex items-center justify-between mt-4">
@@ -53,61 +51,147 @@
 
         <a
           href="#"
-          class="text-xs text-center text-gray-500 uppercase dark:text-gray-400 hover:underline"
-          >login with email</a
+          class="
+            text-xs text-center text-gray-500
+            uppercase
+            dark:text-gray-400
+            hover:underline
+          "
+          >Connexion avec email</a
         >
 
         <span class="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
       </div>
-
-      <div class="mt-4">
-        <label
-          class="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
-          for="LoggingEmailAddress"
-          >Email Address</label
-        >
-        <input
-          id="LoggingEmailAddress"
-          class="block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-          type="email"
-        />
-      </div>
-
-      <div class="mt-4">
-        <div class="flex justify-between">
+      <form @submit.prevent="connexion">
+        <div class="mt-4">
           <label
-            class="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
-            for="loggingPassword"
-            >Password</label
+            class="
+              block
+              mb-2
+              text-sm
+              font-medium
+              text-gray-600
+              dark:text-gray-200
+            "
+            for="LoggingEmailAddress"
+            >Email Address</label
           >
-          <a
-            href="#"
-            class="text-xs text-gray-500 dark:text-gray-300 hover:underline"
-            >Forget Password?</a
+          <input
+            id="LoggingEmailAddress"
+            class="
+              block
+              w-full
+              px-4
+              py-2
+              text-gray-700
+              bg-white
+              border border-gray-300
+              rounded-md
+              dark:bg-gray-800
+              dark:text-gray-300
+              dark:border-gray-600
+              focus:border-blue-500
+              dark:focus:border-blue-500
+              focus:outline-none
+              focus:ring
+            "
+            type="email"
+            v-model="form.email"
+            v-on:change="mailClick = true"
+          />
+          <p
+            v-if="!mailIsValid && mailClick"
+            class="text-red-500 text-xs italic"
           >
+            Veuillez entrée un email valide
+          </p>
         </div>
 
-        <input
-          id="loggingPassword"
-          class="block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-          type="password"
-        />
-      </div>
+        <div class="mt-4">
+          <div class="flex justify-between">
+            <label
+              class="
+                block
+                mb-2
+                text-sm
+                font-medium
+                text-gray-600
+                dark:text-gray-200
+              "
+              for="loggingPassword"
+              >Password</label
+            >
+            <a
+              href="#"
+              class="text-xs text-gray-500 dark:text-gray-300 hover:underline"
+              >Forget Password?</a
+            >
+          </div>
 
-      <div class="mt-8">
-        <button
-          class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
-        >
-          Connexion
-        </button>
-      </div>
+          <input
+            id="loggingPassword"
+            class="
+              block
+              w-full
+              px-4
+              py-2
+              text-gray-700
+              bg-white
+              border border-gray-300
+              rounded-md
+              dark:bg-gray-800
+              dark:text-gray-300
+              dark:border-gray-600
+              focus:border-blue-500
+              dark:focus:border-blue-500
+              focus:outline-none
+              focus:ring
+            "
+            type="password"
+            v-on:change="passwordClick = true"
+            v-model="form.password"
+          />
+          <p
+            v-if="!passwordIsValid && passwordClick"
+            class="text-red-500 text-xs italic"
+          >
+            Veuillez rentrer un mot de passe (min 6) et (max 24)
+          </p>
+        </div>
 
-      <div class="flex items-center justify-between mt-4">
+        <div class="mt-8">
+          <button
+            class="
+              w-full
+              px-4
+              py-2
+              tracking-wide
+              text-white
+              transition-colors
+              duration-200
+              transform
+              bg-gray-700
+              rounded
+              hover:bg-gray-600
+              focus:outline-none
+              focus:bg-gray-600
+            "
+          >
+            Connexion
+          </button>
+        </div>
+      </form>
+      <!--       <div class="flex items-center justify-between mt-4">
         <span class="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
 
         <a
           href="#"
-          class="text-xs text-gray-500 uppercase dark:text-gray-400 hover:underline"
+          class="
+            text-xs text-gray-500
+            uppercase
+            dark:text-gray-400
+            hover:underline
+          "
           >or sign up</a
         >
 
@@ -115,22 +199,92 @@
       </div>
       <div class="mt-8">
         <button
-          class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-gray-700 rounded hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+          class="
+            w-full
+            px-4
+            py-2
+            tracking-wide
+            text-white
+            transition-colors
+            duration-200
+            transform
+            bg-gray-700
+            rounded
+            hover:bg-gray-600
+            focus:outline-none
+            focus:bg-gray-600
+          "
         >
           Inscription
         </button>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
 
 <script>
+let store = require("store");
+import { mapActions } from "vuex";
 export default {
-  layout: "auth"
+  layout: "auth",
+  data() {
+    return {
+      regMail:
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,24}))$/,
+      passwordClick: false,
+      mailClick: false,
+      form: {
+        email: "",
+        password: "",
+      },
+      err: "",
+    };
+  },
+  computed: {
+    mailIsValid() {
+      return this.regMail.test(this.form.email);
+    },
+    passwordIsValid() {
+      return this.form.password.length >= 6 && this.form.password.length <= 32;
+    },
+    formIsValid() {
+      return this.mailIsValid && this.passwordIsValid;
+    },
+  },
+  methods: {
+    ...mapActions({ login: "admin/login", reconnect: "admin/reconnection" }),
+
+    async connexion() {
+      if (this.formIsValid) {
+        console.log(this.form);
+        const client = await this.login(this.form);
+        console.log(client);
+        if (client.data) {
+          console.log(client);
+          this.$router.push("/admin");
+        }
+      } else {
+        console.log("invalid form");
+      }
+    },
+    async getAdLogged() {
+      if (store.get("tokenAd")) {
+        this.$router.push("/admin");
+        const client = await this.reconnect(store.get("tokenAd"));
+
+        if (client.data.staff) {
+        }
+      } else {
+      }
+    },
+  },
+  mounted() {
+    this.getAdLogged();
+  },
 };
 </script>
 
-<style>
+<style lang="postcss">
 .t {
   background: center/90% no-repeat url("@/assets/img/Fichier1.png");
 }
