ed9d224483bacb47af02e2ef80142ba4273ba397
bertyn <bertynboulikou@gmail.com>
2021-07-05 10:58:07 +0200

    ajout de lien admin blog

--- components/admin/TheSidebar.vue
+++ components/admin/TheSidebar.vue
@@ -131,7 +131,7 @@
           <li class="items-center">
             <nuxt-link
               class="text-gray-700 hover:text-accent-400 flex items-center text-xs uppercase py-3 font-bold "
-              to="/admin/Ingredients"
+              to="/admin/ingredients"
               ><svg class="opacity-75 h-4 w-4 mr-2 text-sm" viewBox="0 0 24 24">
                 <path
                   fill="currentColor"
@@ -160,6 +160,19 @@
                 />
               </svg>
               Gestion Website</nuxt-link
+            >
+          </li>
+          <li class="items-center">
+            <nuxt-link
+              class="text-gray-700 hover:text-accent-400 flex items-center text-xs uppercase py-3 font-bold "
+              to="/admin/blog"
+              ><svg class="opacity-75 h-4 w-4 mr-2 text-sm" viewBox="0 0 24 24">
+                <path
+                  fill="currentColor"
+                  d="M19 5V19H5V5H19M21 3H3V21H21V3M17 17H7V16H17V17M17 15H7V14H17V15M17 12H7V7H17V12Z"
+                />
+              </svg>
+              Gestion Blog</nuxt-link
             >
           </li>
           <!--  <li class="items-center">
