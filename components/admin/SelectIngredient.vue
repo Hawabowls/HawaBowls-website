ee343ab985a49232f2c969c61cdfc5b1754b1a2d
bertyn99 <bboulikou@gmail.com>
2021-07-06 06:43:34 +0200

    changement du boutton en div

--- components/admin/SelectIngredient.vue
+++ components/admin/SelectIngredient.vue
@@ -1,15 +1,35 @@
 <template
-  ><!-- This example requires Tailwind CSS v2.0+ -->
+  >
+  <!-- This example requires Tailwind CSS v2.0+ -->
   <div class="relative inline-block text-left">
     <div>
       <div
         type="button"
-        class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500"
+        class="
+          flex
+          justify-center
+          w-full
+          rounded-md
+          border border-gray-300
+          shadow-sm
+          px-4
+          py-2
+          bg-white
+          text-sm
+          font-medium
+          text-gray-700
+          hover:bg-gray-50
+          focus:outline-none
+          focus:ring-2
+          focus:ring-offset-2
+          focus:ring-offset-gray-100
+          focus:ring-indigo-500
+        "
         id="menu-button"
         aria-expanded="true"
         aria-haspopup="true"
       >
-        Options
+        Selectionnner vos Ingredients
         <!-- Heroicon name: solid/chevron-down -->
         <svg
           class="-mr-1 ml-2 h-5 w-5"
@@ -39,7 +59,20 @@
   -->
     <div
       v-if="open"
-      class="origin-top-right z-30 absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none"
+      class="
+        origin-top-right
+        z-30
+        absolute
+        right-0
+        mt-2
+        w-64
+        rounded-md
+        shadow-lg
+        bg-white
+        ring-1 ring-black ring-opacity-5
+        divide-y divide-gray-100
+        focus:outline-none
+      "
       role="menu"
       aria-orientation="vertical"
       aria-labelledby="menu-button"
@@ -61,126 +94,6 @@
           {{ i.name }}
         </div>
       </div>
-      <div class="py-1" role="none">
-        <a
-          href="#"
-          class="text-gray-700 group flex items-center px-4 py-2 text-sm"
-          role="menuitem"
-          tabindex="-1"
-          id="menu-item-2"
-        >
-          <!-- Heroicon name: solid/archive -->
-          <svg
-            class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path d="M4 3a2 2 0 100 4h12a2 2 0 100-4H4z" />
-            <path
-              fill-rule="evenodd"
-              d="M3 8h14v7a2 2 0 01-2 2H5a2 2 0 01-2-2V8zm5 3a1 1 0 011-1h2a1 1 0 110 2H9a1 1 0 01-1-1z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Archive
-        </a>
-        <a
-          href="#"
-          class="text-gray-700 group flex items-center px-4 py-2 text-sm"
-          role="menuitem"
-          tabindex="-1"
-          id="menu-item-3"
-        >
-          <!-- Heroicon name: solid/arrow-circle-right -->
-          <svg
-            class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Move
-        </a>
-      </div>
-      <div class="py-1" role="none">
-        <a
-          href="#"
-          class="text-gray-700 group flex items-center px-4 py-2 text-sm"
-          role="menuitem"
-          tabindex="-1"
-          id="menu-item-4"
-        >
-          <!-- Heroicon name: solid/user-add -->
-          <svg
-            class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z"
-            />
-          </svg>
-          Share
-        </a>
-        <a
-          href="#"
-          class="text-gray-700 group flex items-center px-4 py-2 text-sm"
-          role="menuitem"
-          tabindex="-1"
-          id="menu-item-5"
-        >
-          <!-- Heroicon name: solid/heart -->
-          <svg
-            class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Add to favorites
-        </a>
-      </div>
-      <div class="py-1" role="none">
-        <a
-          href="#"
-          class="text-gray-700 group flex items-center px-4 py-2 text-sm"
-          role="menuitem"
-          tabindex="-1"
-          id="menu-item-6"
-        >
-          <!-- Heroicon name: solid/trash -->
-          <svg
-            class="mr-3 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-              clip-rule="evenodd"
-            />
-          </svg>
-          Delete
-        </a>
-      </div>
     </div>
   </div>
 </template>
@@ -190,7 +103,7 @@ export default {
   props: ["ing", "selectedIngredient"],
   data() {
     return {};
-  }
+  },
 };
 </script>
 
