8bce72c48a0c637d27c22aa2b669e23f3dbbac26
bertyn <bertynboulikou@gmail.com>
2021-07-06 22:18:09 +0200

    création du composant d'upload

---
+++ components/admin/UploadCloudinary.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="space-y-1 text-center">
+    <svg
+      class="mx-auto h-12 w-12 text-gray-400"
+      stroke="currentColor"
+      fill="none"
+      viewBox="0 0 48 48"
+      aria-hidden="true"
+    >
+      <path
+        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+    <div class="flex text-sm text-gray-600">
+      <label
+        for="file-upload"
+        class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+      >
+        <span>Upload a file</span>
+        <input
+          id="file-upload"
+          name="file-upload"
+          :multiple="nb > 1"
+          type="file"
+          class="sr-only"
+        />
+      </label>
+      <p class="pl-1">or drag and drop</p>
+    </div>
+    <p class="text-xs text-gray-500">
+      PNG, JPG, Webp up to 10MB
+    </p>
+  </div></template
+>
+
+<script>
+const STATUS_INITIAL = 0,
+  STATUS_SAVING = 1,
+  STATUS_SUCCESS = 2,
+  STATUS_FAILED = 3;
+export default {
+  props: ["uploads", "nb"],
+  data() {
+    return {
+      uploadedFiles: [],
+      uploadError: null,
+      currentStatus: null,
+      uploadFieldName: "photos"
+    };
+  },
+  computed: {
+    isInitial() {
+      return this.currentStatus === STATUS_INITIAL;
+    },
+    isSaving() {
+      return this.currentStatus === STATUS_SAVING;
+    },
+    isSuccess() {
+      return this.currentStatus === STATUS_SUCCESS;
+    },
+    isFailed() {
+      return this.currentStatus === STATUS_FAILED;
+    }
+  },
+  methods: {
+    reset() {
+      // reset form to initial state
+      this.currentStatus = STATUS_INITIAL;
+      this.uploadedFiles = [];
+      this.uploadError = null;
+    }
+  },
+  mounted() {
+    this.reset();
+  }
+};
+</script>
+
+<style></style>
