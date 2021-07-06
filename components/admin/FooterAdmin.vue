33249d4045dacb07f023c9dd824449ae0dc877a3
bertyn99 <bboulikou@gmail.com>
2021-07-06 06:16:05 +0200

    fix property date

--- components/admin/FooterAdmin.vue
+++ components/admin/FooterAdmin.vue
@@ -7,21 +7,35 @@
       >
         <div class="w-full md:w-4/12 px-4">
           <div class="text-sm text-blueGray-500 font-semibold py-1">
-            Copyright © {{ date }}
+            Copyright © 2021
             <a
               href="https://www.creative-tim.com"
-              class="text-blueGray-500 hover:text-blueGray-700 text-sm font-semibold py-1"
+              class="
+                text-blueGray-500
+                hover:text-blueGray-700
+                text-sm
+                font-semibold
+                py-1
+              "
             >
               Hawabowl's
             </a>
           </div>
         </div>
         <div class="w-full md:w-8/12 px-4">
-          <ul class="flex flex-wrap list-none md:justify-end  justify-center">
+          <ul class="flex flex-wrap list-none md:justify-end justify-center">
             <li>
               <a
                 href=""
-                class="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
+                class="
+                  text-blueGray-600
+                  hover:text-blueGray-800
+                  text-sm
+                  font-semibold
+                  block
+                  py-1
+                  px-3
+                "
               >
                 A propos
               </a>
@@ -29,14 +43,30 @@
             <li>
               <a
                 href=""
-                class="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
+                class="
+                  text-blueGray-600
+                  hover:text-blueGray-800
+                  text-sm
+                  font-semibold
+                  block
+                  py-1
+                  px-3
+                "
               >
               </a>
             </li>
             <li>
               <a
                 href="http://blog.creative-tim.com"
-                class="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
+                class="
+                  text-blueGray-600
+                  hover:text-blueGray-800
+                  text-sm
+                  font-semibold
+                  block
+                  py-1
+                  px-3
+                "
               >
                 Blog
               </a>
@@ -44,7 +74,15 @@
             <li>
               <a
                 href=""
-                class="text-blueGray-600 hover:text-blueGray-800 text-sm font-semibold block py-1 px-3"
+                class="
+                  text-blueGray-600
+                  hover:text-blueGray-800
+                  text-sm
+                  font-semibold
+                  block
+                  py-1
+                  px-3
+                "
               >
               </a>
             </li>
